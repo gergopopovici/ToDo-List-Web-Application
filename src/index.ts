da72c4978de6bf4a449064b421bdfd407c0e9905
gergopopovici <popovicigergo0330@gmail.com>
2024-10-20 23:25:31 +0300

    Created a function to read Tree JSON objects and set up Prettier for TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
-class Tree<T>{
-    value:T;
-    children:Tree<T>[];
-    constructor(value:T){
-        this.value = value;
-        this.children = [];
+class Tree<T> {
+  value: T;
+  children: Tree<T>[];
+  constructor(value: T) {
+    this.value = value;
+    this.children = [];
+  }
+  addChildren(child: Tree<T>): void {
+    this.children.push(child);
+  }
+}
+function readTreeJson<T>(json): Tree<T> {
+  let newTree = new Tree<T>(json.value);
+  let treeArray = [{ node: newTree, json: json }];
+  while (treeArray.length > 0) {
+    let current = treeArray.pop();
+    if (current) {
+      for (let child of current?.json.children) {
+        let newChild = new Tree<T>(child.value);
+        treeArray.push({ node: newChild, json: child });
+        current.node.addChildren(newChild);
+      }
     }
-    addChildren(child: Tree<T>): void {
-        this.children.push(child);
-    }
+  }
+  return newTree;
 }
