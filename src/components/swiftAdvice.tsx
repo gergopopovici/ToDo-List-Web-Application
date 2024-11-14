fc1c157cfe673b64f64bb944e4b44563ad94e5d0
gergopopovici <popovicigergo0330@gmail.com>
2024-11-14 11:42:49 +0200

    Added the Search feature for the app

--- src/components/swiftAdvice.tsx
+++ src/components/swiftAdvice.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import { fetchRandomAdvice } from '../Api/api';
-import { Button, Typography, Switch, Stack } from '@mui/material';
+import { fetchRandomAdvice, fetchAdviceByKeyword } from '../Api/api';
+import { Button, Typography, Switch, Stack, TextField } from '@mui/material';
 import AdviceCard from './adviceCard';
 import { Advice } from '../interfaces/advice';
 
 const SwiftAdvice: React.FC = () => {
-  console.log('Szar');
   const [advice, setAdvice] = React.useState<Advice[]>([]);
   const [favorites, setFavorites] = React.useState<Set<number>>(new Set());
   const [showFavorites, setShowFavorites] = React.useState(false);
   const [errorMessage, setErrorMessage] = React.useState('');
+  const [keyword, setSearchKeyword] = React.useState('');
 
   const handleGetRandomAdvice = async () => {
     const result = await fetchRandomAdvice();
     if (result) {
       setAdvice((prev) => prev.concat(result));
+    }
+  };
+  const handleGetAdviceByKeyword = async (keyword: string) => {
+    const result = await fetchAdviceByKeyword(keyword);
+    if (result.slips) {
+      setAdvice(result.slips);
+      setErrorMessage('');
+    } else {
+      setAdvice([]);
+      setErrorMessage(`No advice found for keyword "${keyword}"`);
     }
   };
   const handleToggleFavorite = (id: number) => {
@@ -32,6 +42,8 @@ const SwiftAdvice: React.FC = () => {
     <div className="container">
       <h1>Advices</h1>
       <div className="controls">
+        <TextField label="Search Advice" value={keyword} onChange={(e) => setSearchKeyword(e.target.value)} />
+        <Button onClick={() => handleGetAdviceByKeyword(keyword)}>Search</Button>
         <Switch checked={showFavorites} onChange={(e) => setShowFavorites(e.target.checked)} color="primary" />
         <Button onClick={handleGetRandomAdvice}>Get Random Advice</Button>
       </div>
