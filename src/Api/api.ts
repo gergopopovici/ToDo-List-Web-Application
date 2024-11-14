356865f42f10e5f7980aebf3063cd3ba61b74c76
gergopopovici <popovicigergo0330@gmail.com>
2024-11-14 11:26:34 +0200

    Add fetchAdviceByKeyword

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -8,3 +8,13 @@ export const fetchRandomAdvice = async () => {
     return null;
   }
 };
+export const fetchAdviceByKeyword = async (keyword: string) => {
+  try {
+    const response = await fetch(`https://api.adviceslip.com/advice/search/${keyword}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data: ', error);
+    return null;
+  }
+};
