10085d9b1341c207f3bb915f7ceab28954e1bda1
gergopopovici <popovicigergo0330@gmail.com>
2026-02-17 03:25:42 +0200

    Update API URL configuration for production environment

--- src/config/apiURl.ts
+++ src/config/apiURl.ts
@@ -1,5 +1,7 @@
-const api = 'http://localhost:8080';
+// const api = 'http://localhost:8080'; // Local development
+const api = 'https://todo-list-backend-z0zq.onrender.com'; // Production
 const apiURL = `${api}/api`;
 const loginApiUrl = `${apiURL}/auth/login`;
 const userApiUrl = `${apiURL}/users`;
+
 export { apiURL, loginApiUrl, userApiUrl };
