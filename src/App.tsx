71350dfc6d25dd7dc73ede26886d8b69fbb21a17
gergopopovici <popovicigergo0330@gmail.com>
2026-02-17 07:35:45 +0200

    Add backend wake-up functionality in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import axios from 'axios';
 import ListToDo from './pages/ListToDo';
 import ToDoEntry from './pages/ToDoEntry';
 import ToDoForm from './pages/ToDoForm';
@@ -13,10 +14,26 @@ import './i18n';
 import LanguageProvider from './context/LanguageContext';
 import { UserRedirect } from './hooks/userRedirect';
 import ListUsers from './pages/ListUsers';
+import { apiURL } from './config/apiURl';
 
 const queryClient = new QueryClient();
 
 function App() {
+  useEffect(() => {
+    const wakeUpBackend = async () => {
+      try {
+        // the attempt itself starts the Render server.
+        console.log('Pinging server to wake it up...');
+        await axios.get(`${apiURL}/todos`);
+      } catch (error) {
+        // Ignore errors here; we expect a 401 if not logged in.
+        // The goal is just to hit the server.
+        console.log('Server pinged.');
+      }
+    };
+
+    wakeUpBackend();
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
